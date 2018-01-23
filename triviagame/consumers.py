778a81d422339e6d36036abf8de4175a36b22516
Joshua Lovins <12jdlovins@gmail.com>
2018-01-23 09:14:21 -0500

    accept connections

--- triviagame/consumers.py
+++ triviagame/consumers.py
@@ -5,6 +5,7 @@ from channels.sessions import channel_session
 @channel_session
 def ws_connect(message):
     print("Client connecting")
+    message.reply_channel.send({"accept": True})
 
 
 @channel_session
